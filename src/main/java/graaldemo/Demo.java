32f0e67a13780daaa9d3f3880a497af6c4cf6f60
Christian Kohlschütter <christian@kohlschutter.com>
2023-01-09 18:37:00 +0100

    Update Demo code, add demo resource and GraalVM config

--- src/main/java/graaldemo/Demo.java
+++ src/main/java/graaldemo/Demo.java
@@ -1,27 +1,74 @@
 package graaldemo;
 
+import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
 
 import org.eclipse.jetty.util.resource.Resource;
 
 public class Demo
 {
-    public static void main( String[] args ) throws URISyntaxException
+    public static void main(String[] args) throws URISyntaxException
     {
         String versionResourceRef = "org/eclipse/jetty/version/build.properties";
         System.out.println("-- Looking for: " + versionResourceRef);
         URL urlVersion = Thread.currentThread().getContextClassLoader().getResource(versionResourceRef);
-        if (urlVersion == null)
-            System.out.println("WARNING: Unable to find resource: " + versionResourceRef);
-        else
-            System.out.println("FOUND: " + urlVersion.toURI());
+        dumpURLinfo(urlVersion);
 
+        System.out.println();
         System.out.println("-- Looking for: PathResource.class");
         URL urlPathResource = Resource.class.getResource("PathResource.class");
-        if (urlPathResource == null)
-            System.out.println("WARNING: Unable to find PathResource.class");
-        else
-            System.out.println("FOUND: " + urlPathResource.toURI());
+        dumpURLinfo(urlPathResource);
+
+        System.out.println();
+        System.out.println("-- Looking for: hello.txt relative to " + Demo.class.getName());
+        URL urlDemoResource = Demo.class.getResource("hello.txt");
+        dumpURLinfo(urlDemoResource);
+    }
+
+    private static void dumpURLinfo(URL url) throws URISyntaxException
+    {
+        if (url == null)
+        {
+            System.out.println("WARNING: Unable to find resource");
+            return;
+        }
+        URI uri = url.toURI();
+        System.out.println("FOUND:              " + uri);
+        Path p;
+        try
+        {
+            p = Path.of(uri);
+        }
+        catch (FileSystemNotFoundException e)
+        {
+            // This is "somewhat" expected...
+            // thrown for jar:file: and resource: URIs upon first interaction with container
+            e.printStackTrace();
+            try
+            {
+                FileSystems.newFileSystem(uri, Collections.emptyMap());
+                p = Path.of(uri);
+            }
+            catch (RuntimeException | IOException e1)
+            {
+                e.printStackTrace();
+                p = null;
+            }
+        }
+        System.out.println("Path:               " + p + " (exists:" + Files.exists(p) + ")");
+        if (p != null)
+        {
+            URI pathUri = p.toUri();
+            System.out.println("Path.toUri:         " + pathUri);
+            Path p2 = Path.of(pathUri);
+            System.out.println("Path of Path.toUri: " + p2 + " (exists:" + Files.exists(p2) + ")");
+        }
     }
 }
